d7958f08cc677ecb720a93e7fd906854b22f3722
matus <matussadovsky99@gmail.com>
2025-04-30 21:36:02 +0200

    funguj

--- web.py
+++ web.py
@@ -6,4 +6,5 @@ st.title("kokot")
 #podnatpis
 st.subheader("si kokot")
 st.write("igor je kokot")
+st.image("https://cmesk-ott-images-tvnoviny.ssl.cdn.cra.cz/r696x392n/8a5e5d44-dc81-406a-9dac-c8458b6eb681")
 #kontext
