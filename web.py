ac666f42631ea0e27e5c5fe19e4f0086adbd1a29
matus <matussadovsky99@gmail.com>
2025-04-30 21:22:12 +0200

    funguj

---
+++ web.py
@@ -0,0 +1,9 @@
+import streamlit
+st=streamlit
+#toto je nayov v kornom rohu
+st.set_page_config(page_title="kokot",layout="wide")
+st.title("kokot")
+#podnatpis
+st.subheader("si kokot")
+st.write("igor je kokot")
+#kontext
