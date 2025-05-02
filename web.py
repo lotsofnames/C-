c84d0dfcb405ea652d9f33da09dcef1358697ef0
matus <matussadovsky99@gmail.com>
2025-05-02 14:28:38 +0200

    funguj

--- web.py
+++ web.py
@@ -1,10 +1,13 @@
 import streamlit
-st=streamlit
-#toto je nayov v kornom rohu
-st.set_page_config(page_title="kokot",layout="wide")
+
+st = streamlit
+# toto je nayov v kornom rohu
+st.set_page_config(page_title="kokot", layout="wide")
 st.title("kokot")
-#podnatpis
+# podnatpis
 st.subheader("si kokot")
 st.write("igor je kokot")
-st.image("https://cmesk-ott-images-tvnoviny.ssl.cdn.cra.cz/r696x392n/8a5e5d44-dc81-406a-9dac-c8458b6eb681")
-#kontext
+st.image(
+    "https://cmesk-ott-images-tvnoviny.ssl.cdn.cra.cz/r696x392n/8a5e5d44-dc81-406a-9dac-c8458b6eb681"
+)
+# kontext
