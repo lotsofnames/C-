364c568e2a90d9ef2461517589c04d6fbbb0397d
lotsofnames <116421444+lotsofnames@users.noreply.github.com>
2025-05-05 11:19:30 +0200

    mentalne nestabilni clovek

--- web.py
+++ web.py
@@ -2,11 +2,11 @@ import streamlit
 
 st = streamlit
 # toto je nayov v kornom rohu
-st.set_page_config(page_title="kokot", layout="wide")
-st.title("kokot")
+st.set_page_config(page_title="mentalne nestabilni clovek", layout="wide")
+st.title("mentalne nestabilni clovek")
 # podnatpis
-st.subheader("si kokot")
-st.write("igor je kokot")
+st.subheader("si mentalne nestabilni clovek")
+st.write("igor je mentalne nestabilni clovek")
 st.image(
     "https://cmesk-ott-images-tvnoviny.ssl.cdn.cra.cz/r696x392n/8a5e5d44-dc81-406a-9dac-c8458b6eb681"
 )
