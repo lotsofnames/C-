c6674beecb0eb5b18561352c00f567956e94ccd2
matus <matussadovsky99@gmail.com>
2025-06-01 18:25:03 +0200

    funguj

--- web.py
+++ web.py
@@ -1,13 +1,17 @@
 import streamlit
-
+from word import file
 st = streamlit
+slova = file("Enersol.txt")
 # toto je nayov v kornom rohu
-st.set_page_config(page_title="mentalne nestabilni clovek", layout="wide")
-st.title("mentalne nestabilni clovek")
+
 # podnatpis
-st.subheader("si mentalne nestabilni clovek")
-st.write("igor je mentalne nestabilni clovek")
-st.image(
-    "https://cmesk-ott-images-tvnoviny.ssl.cdn.cra.cz/r696x392n/8a5e5d44-dc81-406a-9dac-c8458b6eb681"
-)
+
+for slovo in slova:
+    if "$" in slovo:
+        st.title(slovo.replace("$", ""))
+    elif slovo.startswith("#"):
+        st.subheader(slovo.replace("#", ""))
+    else:
+        st.write(slovo)
+
 # kontext
