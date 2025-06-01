3ddfafdb5b751397f74a24b9373b969d1c089ab5
matus <matussadovsky99@gmail.com>
2025-06-01 21:36:08 +0200

    funguj

--- web.py
+++ web.py
@@ -2,16 +2,14 @@ import streamlit
 from word import file
 st = streamlit
 slova = file("Enersol.txt")
-# toto je nayov v kornom rohu
-
-# podnatpis
 
 for slovo in slova:
     if "$" in slovo:
         st.title(slovo.replace("$", ""))
     elif slovo.startswith("#"):
         st.subheader(slovo.replace("#", ""))
+    elif slovo.startswith("\\"):
+        st.image(f"{slovo.strip().replace("\\","")}..jpg")
     else:
         st.write(slovo)
 
-# kontext
